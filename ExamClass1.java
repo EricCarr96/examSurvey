15449649131c813e740f72dc5b839cda80a3a331
Eric Colbert-Carr <49103887+EricCarr96@users.noreply.github.com>
2019-04-20 09:58:55 -0400

    Add files via upload
    this is an update to the previous code prompting user input to provide how many number of survey questions, while also providing the number of survey participants. The program will then display the proficiency level and lay it out in a grid format.

--- ExamClass1.java
+++ ExamClass1.java
@@ -1,8 +1,13 @@
+
+import javax.swing.JOptionPane;
+
 /*Created by Eric Colbert-Carr*/
 public class ExamClass1 {
     
     int userID;
     String examTitle;
+    int surveyDiagram[][] = new int[10][10];
+    String sArray[] = new String[10];
     
     
     int createID(){
@@ -11,6 +16,37 @@ public class ExamClass1 {
     return userID;    
         
     }
+    //answer the question
+    void enterS(int sID){
+    
+    sArray[sID] = JOptionPane.showInputDialog("Please enter question:("+sID+")");
+    }
+    //processing the display
+    void sDisplay(int sID){
+    System.out.println("Question("+sID+") : " + sArray[sID]); 
+    }
+    
+    
+    
+    //display the question
+    void uRes(int sID, int uID, int res){
+    surveyDiagram[sID][uID] = res; 
+    }
+    
+    void surveyOutcome(int nI, int nU){
+    
+    for (int sID =0; sID < nI; sID++){
+    for (int uID =0; uID < nU; uID++){
+    System.out.print(surveyDiagram[sID][uID] +"      ");
+    
+    }
+    System.out.println();
+    
+    }
+        
+        
+    }
+    
     
     ExamClass1(){
         
--- ExamRun1.java
+++ ExamRun1.java
@@ -1,18 +1,41 @@
 
+import javax.swing.JOptionPane;
+
+
 public class ExamRun1 {
 
    
     public static void main(String[] args) {
-      
+        
+        int sID, uID = 0;
+        int nI, nU;
+        int res;
+        nI= Integer.parseInt( JOptionPane.showInputDialog("How many questions do you have for the survey?"));
+        nU= Integer.parseInt( JOptionPane.showInputDialog("How many users do you have for the survey?"));
         ExamClass1 poll = new ExamClass1("Customer Satisfaction Survey");
+        for (sID= 0; sID < nI; sID++){
+        poll.enterS(sID);
+        }
+        for(int s = 0; s < nU; s++){
+        sID = poll.createID()-1;
+        System.out.println("User"+sID);
+        for(sID=0; sID < nI; sID++){
+            
+            
+        poll.sDisplay(sID);
+        res = Integer.parseInt(JOptionPane.showInputDialog("Please provide an answer (1-5)"));
+        poll.uRes(sID, uID, res);
+        
+        }
+        
+        }
+      
+     
         
         System.out.println("Exam Title" +poll.examTitle);
+        poll.surveyOutcome(nI, nU);
         
-        
-        for(int i =0; i<10; i++)
-            
-            
-        System.out.println("User ID:"+ (poll.createID()-1));
+       
         
     }
     
